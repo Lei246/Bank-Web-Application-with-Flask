fa94a42f60f406b3a3168ed3f87fedb3c53fa423
Lei Shi <56819056+Lei246@users.noreply.github.com>
2022-02-23 15:49:05 +0100

    show customers and sort

--- app.py
+++ app.py
@@ -1,33 +1,84 @@
-from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
+from flask_user import  UserMixin, UserManager,current_user
+from flask_login import current_user,LoginManager
+from model import * #db, seedData, Customer, Account, User, Role, UserRoles, UserRegistration, Transaction,user_manager
+from flask import Flask, render_template, request, url_for, redirect
+from random import randint
+from flask_user import login_required, roles_required
 
-from model import db, seedData
 
  
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:mypassword@localhost/BankWeb'
+app.config.from_object('config.ConfigDebug')
 db.app = app
 db.init_app(app)
 migrate = Migrate(app,db)
- 
- 
+
+#user_manager.app = app
+#user_manager.init_app(app,db, User)
+
+
 
 @app.route("/")
-def startpage():
-    trendingCategories = Category.query.all()
-    return render_template("index.html", trendingCategories=trendingCategories)
+def indexPage():
+    activePage = "startPage"
+    antalPersoner = Customer.query.count()
+    antalKonto = Account.query.count()
+    return render_template('startPage.html', antalPersoner = antalPersoner, antalKonto = antalKonto, activePage=activePage)
 
-@app.route("/category/<id>")
-def category(id):
-    products = Product.query.all()
-    return render_template("category.html", products=products)
+@app.route("/personer")
+#@login_required
+#@roles_accepted('Customer', 'Admin') # AND # OR
+def personerPage():
+    
+    sortColumn = request.args.get('sortColumn', 'Surname')
+    sortOrder = request.args.get('sortOrder', 'asc')
+    page = int(request.args.get('page', 1))
+
+    searchWord = request.args.get('q','')
+
+    activePage = "personerPage"
+    allaPersoner = Customer.query.filter(
+        Customer.Surname.like('%' + searchWord + '%') | 
+        Customer.City.like('%' + searchWord + '%')  | 
+        Customer.Id.like(searchWord)          )
+
+    if sortColumn == "Surname":
+        if sortOrder == "desc":
+            allaPersoner = allaPersoner.order_by(Customer.Surname.desc())
+        else:
+            allaPersoner = allaPersoner.order_by(Customer.Surname.asc())
+
+    if sortColumn == "City":
+        if sortOrder == "desc":
+            allaPersoner = allaPersoner.order_by(Customer.City.desc())
+        else:
+            allaPersoner = allaPersoner.order_by(Customer.City.asc())
+
+
+    paginationObject = allaPersoner.paginate(page,20,False)
+
+
+    return render_template('personerPage.html', 
+            allaPersoner=paginationObject.items, 
+            page=page,
+            sortColumn=sortColumn,
+            sortOrder=sortOrder,
+            q=searchWord,
+            has_next=paginationObject.has_next,
+            has_prev=paginationObject.has_prev, 
+            pages=paginationObject.pages, 
+            activePage=activePage)
+
+
+
+
 
 
 if __name__  == "__main__":
     with app.app_context():
         upgrade()
-    
-    seedData(db)
+        seedData(db)
     app.run()
 
--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 import barnum
 import random
+from flask_user import  UserMixin, UserManager
 from datetime import datetime  
 from datetime import timedelta  
 
 db = SQLAlchemy()
 
-class Customer(db.Model):
+class Customer(db.Model, UserMixin):
     __tablename__= "Customers"
     Id = db.Column(db.Integer, primary_key=True)
     GivenName = db.Column(db.String(50), unique=False, nullable=False)
@@ -24,6 +25,7 @@ class Customer(db.Model):
 
     Accounts = db.relationship('Account', backref='Customer',
      lazy=True)
+
 
 class Account(db.Model):
     __tablename__= "Accounts"
@@ -46,9 +48,81 @@ class Transaction(db.Model):
     NewBalance = db.Column(db.Integer, unique=False, nullable=False)
     AccountId = db.Column(db.Integer, db.ForeignKey('Accounts.Id'), nullable=False)
 
+""" class UserRegistration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=False, nullable=False)
+    firstname = db.Column(db.String(40), unique=False, nullable=False)
+    lastname = db.Column(db.String(40), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    updates = db.Column(db.Boolean, unique=False, nullable=False)  
 
 
+class User(db.Model, UserMixin):
+    __tablename__= 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+
+    # User authentication information. The collation='NOCASE' is required
+    # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    email_confirmed_at = db.Column(db.DateTime()) 
+
+    password = db.Column(db.String(255), nullable=False, server_default='')
+
+    # User information
+    first_name = db.Column(db.String(100), nullable=False, server_default='')
+    last_name = db.Column(db.String(100), nullable=False, server_default='')
+
+    # Define the relationship to Role via UserRoles
+    roles = db.relationship('Role', secondary='user_roles') """
+
+
+""" def AddRoleIfNotExists(namn:str): 
+    if Role.query.filter(Role.name == namn).first():
+        return
+    role = Role()
+    role.name = namn
+    db.session.add(role)
+    db.session.commit()
+
+
+def AddLoginIfNotExists(email:str, passwd:str, roles:list[str]):
+    if User.query.filter(User.email == email).first():
+        return
+    user = User()
+    user.email=email
+    user.email_confirmed_at=datetime.utcnow()
+    user.password=user_manager.hash_password(passwd)    
+    for roleName in roles:
+        role = Role.query.filter(Role.name == roleName).first()
+        user.roles.append(role)
+
+    db.session.add(user)
+    db.session.commit()
+
+# Define the Role data-model
+class Role(db.Model):
+    __tablename__= 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+# Define the UserRoles association table
+class UserRoles(db.Model):
+    __tablename__= 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+user_manager = UserManager(None, db, User) 
+
+
+ """
 def seedData(db):
+    """ AddRoleIfNotExists("Admin")
+    AddRoleIfNotExists("Customer")
+    AddLoginIfNotExists("admin@example.com", "Hejsan123#",["Admin"])
+    AddLoginIfNotExists("customer@example.com", "Hejsan123#",["Customer"])
+ """
     antal =  Customer.query.count()
     while antal < 5000:
         customer = Customer()
@@ -128,5 +202,7 @@ def seedData(db):
         db.session.commit()
         
         antal = antal + 1
+
+
 
 
